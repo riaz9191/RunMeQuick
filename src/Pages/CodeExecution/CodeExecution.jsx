b2c7ac63062010324b0136ad4404d1bde93901a2
riaz9191 <riazahammed9191@gmail.com>
2024-01-09 12:31:16 +0600

    As a guest/user, I want to see my previous executions and their result [Backlog]

--- src/Pages/CodeExecution/CodeExecution.jsx
+++ src/Pages/CodeExecution/CodeExecution.jsx
@@ -1,10 +1,16 @@
 import { useState, useContext, useEffect } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+// import { toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+import MonacoEditor from "react-monaco-editor";
+import { Link } from "react-router-dom";
+
+
+
 
 const CodeExecution = () => {
   const { user } = useContext(AuthContext);
+  console.log(u)
   const [code, setCode] = useState("");
   const [runtime, setRuntime] = useState("");
   const [executionStatus, setExecutionStatus] = useState(null);
@@ -13,19 +19,31 @@ const CodeExecution = () => {
   const [countdown, setCountdown] = useState(null);
   const [cancelBtnVisible, setCancelBtnVisible] = useState(false);
   const [fetchController, setFetchController] = useState(null);
+  const userEmail = user?.email || "";
+
+
 
   const executeCode = async () => {
     if (!code || !runtime || isButtonDisabled) {
       return;
     }
 
+
+
+
     setExecutionStatus("Queued");
     setExecutionResult(null);
     setIsButtonDisabled(true);
     setCancelBtnVisible(true);
 
+
+
+
     const controller = new AbortController();
     setFetchController(controller);
+
+
+
 
     try {
       const response = await fetch("http://localhost:5000/api/execute", {
@@ -36,6 +54,8 @@ const CodeExecution = () => {
         body: JSON.stringify({
           code,
           runtime,
+          userEmail
+
         }),
         signal: controller.signal,
       });
@@ -44,15 +64,24 @@ const CodeExecution = () => {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
+
+
+
       const result = await response.json();
       console.log(result);
 
+
+
+
       setExecutionStatus(result.status);
+
+
+
 
       if (result.status === "Execution Complete") {
         setExecutionResult(result.result);
         if (!result.result) {
-          toast.error("Something went wrong. Please try again.");
+          // toast.error("Something went wrong. Please try again.");
         }
       }
     } catch (error) {
@@ -61,14 +90,20 @@ const CodeExecution = () => {
       } else {
         console.error("Error executing code:", error);
         setExecutionStatus("Something is Wrong, Try Again!");
-        toast.error("Something went wrong. Please try again.");
+        // toast.error("Something went wrong. Please try again.");
       }
     } finally {
       setCancelBtnVisible(false);
       setFetchController(null);
 
+
+
+
       const cooldownDuration = 5;
       setCountdown(cooldownDuration);
+
+
+
 
       const intervalId = setInterval(() => {
         setCountdown((prevCountdown) => {
@@ -76,9 +111,12 @@ const CodeExecution = () => {
             setIsButtonDisabled(false);
             return null;
           }
-          return prevCountdown - 1;
+          return prevCountdown - 0;
         });
       }, 1000);
+
+
+
 
       setTimeout(() => {
         clearInterval(intervalId);
@@ -91,7 +129,7 @@ const CodeExecution = () => {
     if (fetchController) {
       // Abort the ongoing fetch request
       fetchController.abort();
-      setIsButtonDisabled(false)
+      setIsButtonDisabled(false);
     } else {
       // Handle child process termination logic here (if applicable)
     }
@@ -105,32 +143,53 @@ const CodeExecution = () => {
       return;
     }
 
+
+
+
     const timeoutId = setTimeout(() => {
       setCountdown((prevCountdown) => prevCountdown - 1);
     }, 1000);
 
+
+
+
     return () => clearTimeout(timeoutId);
   }, [countdown]);
+
+
+
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4 mt-16 text-white text-center">
-        --- Code Execution ---
+         Code Execution
       </h1>
       <hr className="my-4 max-w-2xl mx-auto" />
       {user ? (
         <div className="space-y-4">
-          <textarea
+          <MonacoEditor
+            width="100%"
+            height="300"
+            defaultLanguage="javascript"
+            theme="vs-dark"
+            className=""
             value={code}
-            onChange={(e) => setCode(e.target.value)}
-            placeholder="Enter your code here"
-            rows="10"
-            className="w-full p-2 border bg-slate-900 text-white rounded focus:outline-none border-blue-900 focus:border-blue-500"
-          ></textarea>
+            onChange={setCode}
+            options={{
+              selectOnLineNumbers: true,
+              roundedSelection: false,
+              readOnly: false,
+              cursorStyle: "line",
+              automaticLayout: true,
+              minimap: {
+                enabled: false,
+              },
+            }}
+          />
           <select
             value={runtime}
             onChange={(e) => setRuntime(e.target.value)}
-            className="w-full p-2 border rounded focus:outline-none bg-slate-900 text-white border-blue-800 focus:border-blue-500"
+            className="w-1/4 p-2 border rounded focus:outline-none bg-slate-900 text-white border-blue-800 focus:border-blue-500 "
           >
             <option value="">Select Runtime</option>
             <option value="python">Python</option>
@@ -181,23 +240,111 @@ const CodeExecution = () => {
               >
                 Result:
               </label>
-              <textarea
-                id="message"
-                rows="10"
-                disabled
-                placeholder={executionResult}
-                className="block p-2.5 w-full text-sm text-gray-900  border focus:ring-blue-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500  border-green-500 rounded-xl bg-slate-900"
-              ></textarea>
+              <div className="bg-[#1E1E1E] p-4 rounded-lg shadow-md  border-blue-600 border">
+                <code
+                  value={executionResult}
+                  className="text-green-400 font-mono "
+                >
+                  {executionResult}
+                </code>
+              </div>
             </div>
           )}
         </div>
       ) : (
-        <p className="text-center text-white border-2 max-w-xl mx-auto border-blue-600">
-          Please log in to execute code.
-        </p>
+        <div className="space-y-4">
+          <h1 className="text-red-800 text-center text-3xl font-bold font-mono"><span className="hover:underline hover:text-red-600 hover:transition duration-500"><Link to='/login'>Login</Link></span> to save your progress</h1>
+          <MonacoEditor
+            width="100%"
+            height="300"
+            defaultLanguage="javascript"
+            theme="vs-dark"
+            className=""
+            value={code}
+            onChange={setCode}
+            options={{
+              selectOnLineNumbers: true,
+              roundedSelection: false,
+              readOnly: false,
+              cursorStyle: "line",
+              automaticLayout: true,
+              minimap: {
+                enabled: false,
+              },
+            }}
+          />
+          <select
+            value={runtime}
+            onChange={(e) => setRuntime(e.target.value)}
+            className="w-full p-2 border rounded focus:outline-none bg-slate-900 text-white border-blue-800 focus:border-blue-500 "
+          >
+            <option value="">Select Runtime</option>
+            <option value="python">Python</option>
+            <option value="javascript">JavaScript</option>
+            <option value="go">Go</option>
+            <option value="c++">C++</option>
+            <option value="php">PHP</option>
+          </select>
+          <div className="flex gap-4">
+            <button
+              onClick={executeCode}
+              className={`bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue ${
+                isButtonDisabled ? "cursor-not-allowed opacity-50" : ""
+              }`}
+              disabled={isButtonDisabled}
+            >
+              Execute Code
+            </button>
+            {cancelBtnVisible && (
+              <button
+                onClick={cancelExecution}
+                className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none focus:shadow-outline-red"
+              >
+                Cancel
+              </button>
+            )}
+            {countdown !== null && (
+              <div className="text-gray-500 text-sm mt-2 flex gap-2 items-center justify-center">
+                <div>
+                  <div className="three-body">
+                    <div className="three-body__dot"></div>
+                    <div className="three-body__dot"></div>
+                    <div className="three-body__dot"></div>
+                  </div>
+                </div>{" "}
+                Cooldown: {countdown}
+              </div>
+            )}
+          </div>
+          <div className="text-white">
+            <strong>Status:</strong> {executionStatus}
+          </div>
+          {executionResult && (
+            <div className="text-white">
+              <label
+                htmlFor="message"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Result:
+              </label>
+              <div className="bg-slate-900 p-4 rounded-lg shadow-md  border-blue-600 border">
+                <code
+                  value={executionResult}
+                  className="text-green-400 font-mono "
+                >
+                  {executionResult}
+                </code>
+              </div>
+            </div>
+          )}
+        </div>
+       
       )}
     </div>
   );
 };
+
+
+
 
 export default CodeExecution;
---
+++ src/Pages/CodeExecution/PreviousExecutions.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PreviousExecutions = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PreviousExecutions;
